b01ea061847cfe91db8fde08be5c0c5499784458
Zoe Gadon-Thompson <zoe.gadonthompson@farsetlabs.org.uk>
2019-10-02 00:01:05 +0100

    adding functionality for checking carriers & returning errors

--- lookup.php
+++ lookup.php
@@ -4,13 +4,25 @@
 require "vendor/autoload.php";
 
 // Your Account Sid and Auth Token from twilio.com/user/account
-// waiting on text to authorise account
 $sid = "{{account_sid}}";
 $token = "{{ auth_token }}";
 $client = new Lookups_Services_Twilio($sid, $token);
 
-// Make a call to the Lookup API
-$number = $client->phone_numbers->get("15108675309");
+try{
+  $number = $client->phone_numbers->get("07904787677", array("CountryCode" => "UK", "Type" => "carrier"));
+  $number->carrier->type; // Should throw an exception if the number doesn't exist.
+  return true;
+} catch (Exception $e) {
+  // If a 404 exception was encountered return false.
+  if($e->getStatus() == 404) {
+      return false;
+  } else {
+      throw $e;
+  }
+}
 
-// Print the nationally formatted phone number
-echo $number->national_format . "\r\n"; // => (510) 867-5309
+if (isValidNumber("19999999999")) {
+    echo "Phone number is valid";
+} else {
+    echo "Phone number is not valid";
+}// Log the carrier type and name
