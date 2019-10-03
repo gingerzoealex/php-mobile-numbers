89040a4cc777c4242f99dfdcad71d3cba264c375
Zoe Gadon-Thompson <zoe.gadonthompson@farsetlabs.org.uk>
2019-10-03 16:17:44 +0100

    just need to parse csv columns

--- app.php
+++ app.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require "lookup.php";
 
@@ -8,110 +8,57 @@ $csv_file = './phone-numbers.csv';
 //Open the file.
 $fileHandle = fopen($csv_file, "r");
 
-$data = [];
+// $data = [];
 //Loop through the CSV rows.
 
 $pattern = "/^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/";
 
 $checkNumber = new MobileNumberLookup();
 
-$val = getopt(null, ["phone_number:"]);
-if ($val !== false) {
-  $numbers = explode(",",$val['phone_number']);
+$cli_numbers = getopt(null, ["phone_number:"]);
+$cli_csv_name = getopt(null, ["csv:"]);
+// print_r (var_export($cli_csv_name, true));
+if ($cli_numbers !== false) {
+  $numbers = explode(",",$cli_numbers['phone_number']);
   foreach($numbers as $number){
-    $checkNumber->validate_number($number);
+    print "\n";
+    // $checkNumber->validate_number($number);
+    print "\n";
   }
 }
-else {
-	echo "Could not get value of command line option\n";
-}
 
-while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
-  # prints the 4th column which is mobile number. could use regex to parse & find the column.
-    $data = [$row[3]];
-    // print_r($data);
-    foreach($data as $number){
-      sleep(0.5);
-      preg_match_all($pattern,$number,$matches, PREG_PATTERN_ORDER);
-      foreach($matches[0] as $valid){
-        // print_r($str_num);
-        if($valid){
-          $str_num = str_replace(' ', '', $valid);
-          print $str_num . "\n";
-          $checkNumber->validate_number($number);
-        }
-        else{
-          print_r("No match");
+if ($cli_csv_name !== false){
+  $files = explode(",",$cli_csv_name['csv']);
+  foreach($files as $file){
+    $csv = fopen($file, "r");
 
+###
+    $columns = explode(",", $csv[0]);
+    print_r($columns);
+    // $csv_column_names = implode(" ", $columns);
+    // print_r($csv_column_names);
+###
+    
+    while (($row = fgetcsv($csv, 200, ",")) !== FALSE) {
+      # prints the 4th column which is mobile number. could use regex to parse & find the column.
+      $data = [$row[3]];
+      foreach($data as $number){
+        sleep(0.5);
+        preg_match_all($pattern,$number,$matches, PREG_PATTERN_ORDER);
+        foreach($matches[0] as $valid){
+          if($valid){
+            $str_num = str_replace(' ', '', $valid);
+            $checkNumber->validate_number($number);
+          }
+          else{
+            // print_r("No match");
+          }
         }
       }
-
     }
-
-    // foreach($match as $valid_number){
-      // print_r($valid_number . "\n");
-
-    // }
-    // if($match){
-      // echo $data . "\n";
-      // foreach($array as $number){
-        // $validity = $checkNumber->validate_number($number);
-      // }
-    // }
-    // else{
-      // echo "No Match\n";
-    // }
+  }
 }
-
-
-/*
-$checkNumber = new MobileNumberLookup();
-
-$csv = str_getcsv($csv_file, "\n");
-// foreach($csv as $row){
-  // print($row);
+else{
+  print_r("No file selected.");
 }
-
-while (($column = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
-  # prints the 4th column which is mobile number. could use regex to parse & find the column.
-    // print_r(gettype($data));
-    // print_r(gettype($column[3]));
-    // print_r($column[3] . "\n");
-    // $str_num = $column[3] . ",";
-    // print_r(explode(",", $str_num));
-    // $var = array($str_num);
-    // $var = explode(",", $str_num);
-    // print_r(gettype($var));
-    // $str_num = str_replace(' ', '', $column[3]);
-    // print_r($str_num);
-
-    // foreach($column[3] as $row){
-      // $data = $column[3];
-      // print_r($row);
-      // $match = [preg_match($pattern,$data)];
-      // print(gettype($match));
-    // }
-    // $nums_array = explode(",",$match);
-    // if($match){
-      // print($nums_array);
-      // foreach($match as $num){
-        // print(gettype($num));
-
-      // }
-      // echo $data . "\n";
-
-      // foreach($match as $number){
-        // printr($number);
-      // $checkNumber->validate_number($number);
-        // print_r(gettype($number));
-        // $validity = $checkNumber->validate_number($number);
-      // }
-    // }
-    // else{
-      // echo "Not a valid UK mobile number\n";
-    // }
-
-}
-
-
-?> */
+  $checkNumber->create_output_file();
--- lookup.php
+++ lookup.php
@@ -11,6 +11,7 @@ class MobileNumberLookup{
 
     $sid = $_ENV['TWILIO_ACCOUNT_SID'];
     $token = $_ENV['TWILIO_AUTH_TOKEN'];
+    $str_output = "";
 
     try{
       $client = new Lookups_Services_Twilio($sid, $token);
@@ -20,21 +21,46 @@ class MobileNumberLookup{
       $number_type = $number->carrier->type . "\r\n"; // => mobile
       // print gettype($number_type);
       $carrier =  $number->carrier->name; // => Sprint Spectrum, L.P.
-      print "Number:\t" . $phone_number . "\nCarrier:\t" . $carrier . "\nType:\t" . $number_type . "\n_______\n";
+      print_r ("\nNumber:\t" . $phone_number . "\nCarrier:\t" . $carrier . "\nType:\t" . $number_type . "\n_______\n");
+
 
       return true;
     } catch (Exception $e) {
-        // If a 404 exception was encountered return false.
-        print "Number not found: ";
-        print "Error: " . $e->getStatus() . " ";
-        if($e->getStatus() == 404) {
-            return false;
-        } else {
-            return false;
-            // throw $e;
-        }
+      // If a 404 exception was encountered return false.
+      print "Error: " . $e->getStatus() . "\n";
+      if($e->getStatus() == 404) {
+        print " Number is invalid\n";
+        return false;
+      }
+      if($e->getStatus() == 400) {
+        print " Invalid format\n";
+        return false;
+      } else {
+        print "\n";
+        return false;
+      }
+    }
+  }
+
+  function create_output_file(){
+    // open the file "demosaved.csv" for writing
+    $file = fopen('mobile_numbers.csv', 'w');
+    // save the column headers
+    fputcsv($file, array('mobile_number', 'carrier', 'type'));
+
+    // Sample data. This can be fetched from mysql too
+    $data = array(
+    array('Data 11', 'Data 12', 'Data 13'),
+    );
+
+    // save each row of the data
+    foreach ($data as $row)
+    {
+    fputcsv($file, $row);
     }
 
+    // Close the file
+    fclose($file);
   }
 }
 
